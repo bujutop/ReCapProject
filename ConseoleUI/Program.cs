c812c7ecf92ddbce1f6cf3e6fc44e6d1f1719101
YBT <YBT@DESKTOP-6QHQD6V>
2021-02-01 02:42:06 +0300

    Add project files.

---
+++ ConseoleUI/Program.cs
@@ -0,0 +1,82 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConseoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            List<Car> cars = carManager.GetAll();
+
+            //Tüm liste
+            Console.WriteLine("Start List:");
+            cars.ForEach(car => Console.WriteLine("Car Id: {0}, Brand Number: {1}, Color Number: {2}," +
+                " Model Year: {3}, Daily Price: {4}, " +
+                "Description: {5}",car.Id,car.BrandId,car.ColorId,car.ModelYear,car.DailyPrice,car.Description));
+            
+            //Add new car,write new car
+            carManager.Add(new Car { Id = 5, BrandId = 38, ColorId = 100, ModelYear = 1996, DailyPrice = 50, Description = "A white old cheap car" });
+            var newCar = cars.Last();
+            Console.WriteLine("\n" + "Add new car and write only new car");
+            Console.WriteLine("Car Id: {0}, Brand Number: {1}, Color Number: {2}, " +
+                "Model Year: {3}, Daily Price: {4}," +
+                " Description: {5}", newCar.Id, newCar.BrandId, newCar.ColorId, newCar.ModelYear, 
+                newCar.DailyPrice, newCar.Description);
+
+            //Delete car by id number
+            carManager.Delete(cars.SingleOrDefault(c => c.Id == 2));
+            Console.WriteLine("\n" + "Delete Car 2 and write new list");
+            cars.ForEach(car => Console.WriteLine("Car Id: {0}, Brand Number: {1}, Color Number: {2}," +
+                " Model Year: {3}, Daily Price: {4}," +
+                " Description: {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description));
+
+            //get car by id
+            int carID = 5;
+            var getCar = carManager.GetById(carID)[0];
+            Console.WriteLine("\n" + "Get by id");
+            Console.WriteLine("Car Id: {0}, Brand Number: {1}, Color Number: {2}, " +
+                "Model Year: {3}, Daily Price: {4}," +
+                " Description: {5}", getCar.Id, getCar.BrandId, getCar.ColorId, getCar.ModelYear,
+                getCar.DailyPrice, getCar.Description);
+
+
+            //update car 
+            getCar.ColorId = 3;
+            getCar.Description = "zzzzzz";
+            carManager.Update(getCar);
+            Console.WriteLine("\n" + "update car 5");
+            Console.WriteLine("new color: {0} , new description: {1}",getCar.ColorId,getCar.Description);
+
+
+           
+            //Final version
+            Console.WriteLine("\n" +"Final List:");
+            cars.ForEach(car => Console.WriteLine("Car Id: {0}, Brand Number: {1}, Color Number: {2}," +
+                " Model Year: {3}, Daily Price: {4}," +
+                " Description: {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=48,ColorId=201,ModelYear=2020,DailyPrice=1000,Description="Light blue Alfa Romeo Giulia 2020 " },
+                new Car{Id=2,BrandId=45,ColorId=29,ModelYear=2019,DailyPrice=900,Description="Red Alfa Romeo Stelvio 2019" },
+                new Car{Id=3,BrandId=103,ColorId=95,ModelYear=2020,DailyPrice=3500,Description="Silver Aston Martin DB11 2020" },
+                new Car{Id=4,BrandId=2,ColorId=9,ModelYear=2018,DailyPrice=2000,Description="Black BMW X3 2018" }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
